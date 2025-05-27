1ec09f7f2bc65562a292c14196b1030e78fd5119
josecoronel20 <josecoronel20@outlook.com>
2025-05-27 16:36:09 -0300

    fixing issue of styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,19 @@ import animate from 'tailwindcss-animate'
 const config = {
     darkMode: ['class'],
     content: [
-      './src/**/*.{js,ts,jsx,tsx}',
+      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+      './src/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
+    	container: {
+    		center: true,
+    		padding: "2rem",
+    		screens: {
+    			"2xl": "1400px",
+    		},
+    	},
     	extend: {
     		borderRadius: {
     			lg: 'var(--radius)',
@@ -54,7 +64,21 @@ const config = {
     				'4': 'hsl(var(--chart-4))',
     				'5': 'hsl(var(--chart-5))'
     			}
-    		}
+    		},
+    		keyframes: {
+    			"accordion-down": {
+    				from: { height: "0" },
+    				to: { height: "var(--radix-accordion-content-height)" },
+    			},
+    			"accordion-up": {
+    				from: { height: "var(--radix-accordion-content-height)" },
+    				to: { height: "0" },
+    			},
+    		},
+    		animation: {
+    			"accordion-down": "accordion-down 0.2s ease-out",
+    			"accordion-up": "accordion-up 0.2s ease-out",
+    		},
     	}
     },
     plugins: [animate],
