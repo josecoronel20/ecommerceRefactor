b6b9e97cc89900a07ede96bfcb193a6a34460afc
josecoronel20 <josecoronel20@outlook.com>
2025-06-15 09:15:29 -0300

    fix:delete some dependencies :@tailwindcss/postcss critters style-loader,etc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import 'tailwindcss/tailwind.css';
 import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
 import Header from '@/components/layout/header/Header';
