ccb2d9cc8e51d3675c09b559da69a4d5447980ba
josecoronel20 <josecoronel20@outlook.com>
2025-06-15 07:53:55 -0300

    fix:change content of tailwind config

--- next.config.cjs
+++ next.config.cjs
@@ -8,16 +8,6 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
     JWT_SECRET: process.env.JWT_SECRET,
-  },
-  experimental: {
-    optimizeCss: false
-  },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
   }
 };
 
